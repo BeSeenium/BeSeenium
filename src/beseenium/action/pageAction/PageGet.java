e4d2c115b78e3ef7b221a3c3d142fd69196f67b9
Jan Hanson <jpchansondev@gmail.com>
2015-07-22 16:30:14 +0100

    added GPLv3 Licence

---
+++ src/beseenium/action/pageAction/PageGet.java
@@ -0,0 +1,15 @@
+package beseenium.action.pageAction;
+
+import beseenium.action.AbstractAction;
+import beseenium.exception.ActionDataException;
+import beseenium.exception.ActionException;
+
+public class PageGet extends AbstractAction {
+
+	@Override
+	public String execute(int n) throws ActionDataException, ActionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
