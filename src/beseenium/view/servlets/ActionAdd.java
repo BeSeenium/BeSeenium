a58ed9136d450bcba02e8a19a63a91611727705e
Jan Hanson <jpchansondev@gmail.com>
2015-07-30 21:50:48 +0100

    specific servlet templates added

---
+++ src/beseenium/view/servlets/ActionAdd.java
@@ -0,0 +1,22 @@
+package beseenium.view.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ActionAdd extends HttpServlet 
+{
+	/**
+	 * needed to avoid warnings
+	 */
+	private static final long serialVersionUID = 300401747680841856L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException
+	{
+		
+	}
+}
---
+++ src/beseenium/view/servlets/BrowserCapabilitiesServlet.java
@@ -0,0 +1,22 @@
+package beseenium.view.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class BrowserCapabilitiesServlet extends HttpServlet 
+{
+	/**
+	 * need this to avoid warnings
+	 */
+	private static final long serialVersionUID = -1726674989848663443L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException
+	{
+		
+	}
+}
